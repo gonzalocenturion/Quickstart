2919a49ab3162d3edeefe965845d054b8c42b682
gonzalo.centurion.vortigo <gonzalo.centurion.vortigo@gruposuperdigital.com>
2023-03-17 09:38:50 -0300

    Se realiza las configuraciones en IdentityServer para ser consumido por una web app

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -1,13 +1,15 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
+        new List<IdentityResource>
         { 
-            new IdentityResources.OpenId()
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -19,6 +21,7 @@ public static class Config
     public static IEnumerable<Client> Clients =>
         new List<Client>
         {
+            // machine to machine client (from quickstart 1)
             new Client
             {
                 ClientId = "client",
@@ -34,6 +37,26 @@ public static class Config
 
                 // scopes that client has access to
                 AllowedScopes = { "api1" }
+            },
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+            
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                }
             }
         };
 }
